0a9e2408c5c53869f6e670e5435fe48cb95ed3d4
Pankti002 <panktishah8902@gmail.com>
2021-09-23 23:15:59 +0530

    Converter Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+// import { ToastrModule } from 'ngx-toastr';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutComponent } from './about/about.component';
+import { CalcComponent } from './calc/calc.component';
+import { UnitComponent } from './unit/unit.component';
+import { LogoutComponent } from './logout/logout.component';
+import { CurrencyComponent } from './currency/currency.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CalcComponent,
+    CurrencyComponent,
+    LoginComponent,
+    LogoutComponent,
+    SignupComponent,
+    UnitComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule, 
+    // ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent implements OnInit {
+
+  a:any
+  b:any
+  c:number=0
+
+  constructor() 
+  {
+    console.log("constructor...")
+  }
+
+  ngOnInit(): void 
+  {
+    console.log("ngOnInit")
+  }
+
+  addition()
+  {
+    console.log("Addition() called...")
+    console.log("a = >",this.a)
+    console.log("b = >",this.b)
+    this.c = this.a + this.b
+  }
+  subtraction()
+  {
+    console.log("Subtraction() called...")
+    console.log("a = >",this.a)
+    console.log("b = >",this.b)
+    this.c = this.a - this.b
+  }
+  multiplication()
+  {
+    console.log("Multiplication() called...")
+    console.log("a = >",this.a)
+    console.log("b = >",this.b)
+    this.c = this.a * this.b
+  }
+  division()
+  {
+    console.log("Division() called...")
+    console.log("a = >",this.a)
+    console.log("b = >",this.b)
+    this.c = this.a / this.b
+  }
+  
+}
